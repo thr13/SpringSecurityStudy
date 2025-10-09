01ac09e4ff563d5e428bec06fdbc92f7d273e1db
sugwan <skhrnt2945@gmail.com>
2025-10-09 22:19:56 +0900

    feat: user 정보를 담당하는 UserDetailsService

--- src/main/java/com/security/springsecuritymaster/SecurityConfig.java
+++ src/main/java/com/security/springsecuritymaster/SecurityConfig.java
@@ -5,6 +5,10 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @EnableWebSecurity
@@ -17,5 +21,22 @@ public class SecurityConfig {
                 .authorizeHttpRequests(auth -> auth.anyRequest().authenticated())
                 .formLogin(Customizer.withDefaults());
         return http.build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        UserDetails user = User.withUsername("user")
+                .password("{noop}1111")
+                .roles("USER").build();
+
+        UserDetails user2 = User.withUsername("user2")
+                .password("{noop}1111")
+                .roles("USER").build();
+
+        UserDetails user3 = User.withUsername("user3")
+                .password("{noop}1111")
+                .roles("USER").build();
+
+        return new InMemoryUserDetailsManager(user);
     }
 }
