09eeb60df331ebf08dade3de1993ce1b6d7d1871
sugwan <skhrnt2945@gmail.com>
2025-10-09 22:09:10 +0900

    refactor: 중복된 패키지명 수정(security.security -> security.springsecuritymaster)

--- src/main/java/com/security/springsecuritymaster/SecurityApplication.java
+++ src/main/java/com/security/springsecuritymaster/SecurityApplication.java
@@ -1,4 +1,4 @@
-package com.security.security;
+package com.security.springsecuritymaster;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
