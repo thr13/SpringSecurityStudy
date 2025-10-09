78df975d83e5f1e43287db45db2d5b24b50596dd
sugwan <skhrnt2945@gmail.com>
2025-10-09 22:15:42 +0900

    feat: 스프링 시큐리티 설정 - SecurityFilterChain

---
+++ src/main/java/com/security/springsecuritymaster/SecurityConfig.java
@@ -0,0 +1,21 @@
+package com.security.springsecuritymaster;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@EnableWebSecurity
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(auth -> auth.anyRequest().authenticated())
+                .formLogin(Customizer.withDefaults());
+        return http.build();
+    }
+}
