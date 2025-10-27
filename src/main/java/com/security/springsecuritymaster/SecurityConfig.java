7f159c67fb091522c6b21ddf428b0bb424266a92
sugwan <skhrnt2945@gmail.com>
2025-10-27 20:28:40 +0900

    Feat: 폼인증의 로그인(인증) 관련 URL

--- src/main/java/com/security/springsecuritymaster/SecurityConfig.java
+++ src/main/java/com/security/springsecuritymaster/SecurityConfig.java
@@ -2,7 +2,6 @@ package com.security.springsecuritymaster;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.User;
@@ -19,7 +18,23 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .authorizeHttpRequests(auth -> auth.anyRequest().authenticated())
-                .formLogin(Customizer.withDefaults());
+                .formLogin(form -> form
+                        .loginPage("/loginPage") // 로그인 페이지 URL
+                        .loginProcessingUrl("/loginProc") // 아이디, 비밀번호 검증 (로그인 요청 시점, 인증 전) URL
+                        .defaultSuccessUrl("/", true) // 로그인 성공 후 사용자가 이동할 URL, alwaysUse 값이 true 일 경우 로그인 성공시 무조건 defaultSuccessUrl 이 설정한 URL 로 이동하게 된다 (false 일 경우, 인증에 성공시 이전 위치로 redirect 된다)
+                        .failureUrl("/failed") // 인증에 실패할 경우 사용자가 이동할 URL
+                        .usernameParameter("userId") // 인증에 아이디를 확인하는 HTTP 매개변수 설정(기본값: username)
+                        .passwordParameter("passwd") // 인증에 비밀번호를 확인하는 HTTP 매개변수(기본값: password)
+                        .successHandler((request, response, authentication) -> {
+                            System.out.println("authentication: " + authentication);
+                            response.sendRedirect("/home"); // 인증 성공시 이동할 URL
+                        })
+                        .failureHandler((request, response, exception) -> {
+                            System.out.println("exception: " + exception.getMessage()); // 인증이 실패할 경우 AuthenticationException 이 발생한다
+                            response.sendRedirect("/login"); // 인증 실패시 이동할 URL
+                        })
+                        .permitAll()
+                );
         return http.build();
     }
 
