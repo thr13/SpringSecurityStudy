fa22047ca3f43bfada608814bc6299d0e82fe6ee
sugwan <skhrnt2945@gmail.com>
2025-11-03 22:22:50 +0900

    Feat: SecurityContext 와 SecurityContextHolder

--- src/main/java/com/security/springsecuritymaster/IndexController.java
+++ src/main/java/com/security/springsecuritymaster/IndexController.java
@@ -1,17 +1,28 @@
 package com.security.springsecuritymaster;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.security.authentication.AnonymousAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.annotation.CurrentSecurityContext;
 import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@RequiredArgsConstructor
 @RestController
 public class IndexController {
 
+    private final SecurityContextService securityContextService;
+
     @GetMapping("/")
     public String index() {
+        SecurityContext securityContext = SecurityContextHolder.getContextHolderStrategy().getContext();
+        Authentication authentication = securityContext.getAuthentication();
+        System.out.println("authentication = " + authentication);
+
+        securityContextService.securityContext();
+
         return "index";
     }
 
